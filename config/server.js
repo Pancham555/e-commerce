d1245c19dbcc286dc71e70337206edfb91f4bff5
Pancham <panchamb63@gmail.com>
2022-11-10 19:03:31 +0530

    Removed errors

--- config/server.js
+++ config/server.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', process.env.HOST || '0.0.0.0'),
   port: env.int('PORT', process.env.PORT || 1337),
+  admin: {
+    secret: env(process.env.ADMIN_JWT)
+  },
 });
