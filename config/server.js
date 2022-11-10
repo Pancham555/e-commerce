79efde0e98567fce8346b483ab09719b25a234e4
Pancham <panchamb63@gmail.com>
2022-11-10 19:30:18 +0530

    Check commit 2

--- config/server.js
+++ config/server.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
+  url: "https://strapi-ecommerce-7b0b.onrender.com",
   host: env('HOST', process.env.HOST || '0.0.0.0'),
   port: env.int('PORT', process.env.PORT || 1337),
   admin: {
