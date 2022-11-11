5a4f768c982270a0ff6f69a4960f7996c153fa6f
Pancham555 <75246178+Pancham555@users.noreply.github.com>
2022-11-11 10:20:34 +0530

    Added dynamic url

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: "https://strapi-ecommerce-mla2.onrender.com",
+  url: process.env.URL,
   host: env('HOST', process.env.HOST || '0.0.0.0'),
   port: env.int('PORT', process.env.PORT || 1337),
   admin: {
